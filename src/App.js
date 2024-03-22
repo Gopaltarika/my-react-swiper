0c6f64d25d0d3ce5a813d39e777990c2a1e6a932
GopalAror <punjabijanu586@gmail.com>
2024-03-22 15:17:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Navigation, Pagination, A11y } from 'swiper/modules';
 import './App.css';
-
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import slide from "./assets/images/slide1.png";
 function App() {
+  const swiper = useSwiper();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className='max-w-[1140px] mx-auto px-3'>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          loop={true}
+          navigation
+          pagination={{ clickable: true }}
+          modules={[Navigation, Pagination, A11y]}
+          breakpoints={{
+            576: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+            },
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <SwiperSlide><img className='w-full' src={slide} alt='img-slide' /></SwiperSlide>
+          <SwiperSlide><img className='w-full' src={slide} alt='img-slide' /></SwiperSlide>
+          <SwiperSlide><img className='w-full' src={slide} alt='img-slide' /></SwiperSlide>
+          <SwiperSlide><img className='w-full' src={slide} alt='img-slide' /></SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
